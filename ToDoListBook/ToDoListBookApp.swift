e970b7d01f0cfd223d50f88ee2c80f0d3dfebce6
Alex Sobolev <31108@mail.ru>
2024-05-08 09:57:43 +0300

    Initial Commit

---
+++ ToDoListBook/ToDoListBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListBookApp.swift
+//  ToDoListBook
+//
+//  Created by Александр Соболев on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
