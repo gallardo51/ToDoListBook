b91b82a2c3d060edc83b89aa3ca3cc2bb5a264f3
Alex Sobolev <31108@mail.ru>
2024-05-08 10:05:25 +0300

    1. Create app 2. Create core data storage 3. Setup Persistence

---
+++ ToDoListBook/Persistence.swift
@@ -0,0 +1,23 @@
+//
+//  Persistence.swift
+//  ToDoListBook
+//
+//  Created by Александр Соболев on 08.05.2024.
+//
+
+import CoreData
+
+struct PersistentController {
+    
+    static let shared = PersistentController()
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "ToDo")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("unresolved error: \(error)")
+            }
+        }
+    }
+}
